6b0b1721acfb7e20e28cba95ce226303823d11ae
Patrick Jaap <patrick.jaap@wias-berlin.de>
2025-10-29 14:12:10 +0100

    Fix CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,25 +8,25 @@ using LinearAlgebra
     cA = make_const(A)
 
     @test size(cA) == size(A)
-    @test A[3,3] == cA[3,3]
+    @test A[3, 3] == cA[3, 3]
     @test det(A) == det(cA)
     @test sum(A) == sum(cA)
 
-    @test_throws ErrorException cA[4,4] = 42
+    @test_throws ErrorException cA[4, 4] = 42
 end
 
 
 @testset "Dictionaries" begin
 
-   d = Dict(
-    :a => 1,
-    "foo" => "bar"
-   )
+    d = Dict(
+        :a => 1,
+        "foo" => "bar"
+    )
 
-   dd = Dict(
-    :d => d,
-    :b => zeros(3)
-   )
+    dd = Dict(
+        :d => d,
+        :b => zeros(3)
+    )
 
     cdd = make_const(dd)
 
